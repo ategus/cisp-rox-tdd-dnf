b8751a7bbb514a1eccf4e2054e89b736e80b14fe
Ronny Schellenberg <ronny.schellenberg@gmail.com>
2022-07-28 18:11:49 +0200

    serializer added

--- django-puppy-store/puppy_store/puppies/urls.py
+++ django-puppy-store/puppy_store/puppies/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('puppies.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/puppies/<pk>',
+         views.get_delete_update_puppy, name='get_delete_update_puppy'),
+    path('api/v1/puppies/', views.get_post_puppies, name='get_post_puppies')
 ]
